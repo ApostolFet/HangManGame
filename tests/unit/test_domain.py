c08a8b1207e61ef6a817cd0f9760f16d65405131
Apostol Fet <maksim170901@gmail.com>
2024-08-25 22:36:28 +0300

    feat: add tests domain with pytest

---
+++ tests/unit/test_domain.py
@@ -0,0 +1,129 @@
+import pytest
+
+
+from hangman.domain.entity import HangManGame, GameState
+from hangman.domain.exceptions import LetterAlredyGuessError
+
+
+def test_hangman_guess_letter():
+    expected_indeces_guessed_letters = {0, 3}
+
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+    result_guess = game.guess("t")
+    result_indeces_guessed_letters = game.indeces_guessed_letters
+
+    assert result_guess
+    assert result_indeces_guessed_letters == expected_indeces_guessed_letters
+
+
+def test_hangman_not_guess_letter():
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+
+    result_guess = game.guess("d")
+    result_indeces_guessed_letters = game.indeces_guessed_letters
+
+    assert not result_guess
+    assert not result_indeces_guessed_letters
+
+
+def test_hangman_used_letters():
+    expected_used_letter = {"t", "e", "q"}
+
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+    for letter in expected_used_letter:
+        game.guess(letter)
+    result_used_letter = game.used_letters
+
+    assert expected_used_letter == result_used_letter
+
+
+def test_hangman_letter_already_guess():
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+    game.guess("t")
+
+    with pytest.raises(LetterAlredyGuessError):
+        game.guess("t")
+
+
+def test_hangman_count_error():
+    expected_count_error = 3
+
+    game = HangManGame(
+        word="test",
+        max_error=expected_count_error + 1,
+    )
+    game.guess("w")
+    game.guess("r")
+    game.guess("o")
+    result_count_error = game.count_error
+
+    assert expected_count_error == result_count_error
+
+
+def test_hangman_word():
+    expected_word = "test"
+
+    game = HangManGame(
+        word=expected_word,
+        max_error=5,
+    )
+    result_word = game.word
+
+    assert expected_word == result_word
+
+
+def test_hangman_win_game():
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+
+    game.guess("t")
+    game.guess("e")
+    game.guess("s")
+
+    assert game.game_state is GameState.VICTORY
+
+
+def test_hangman_defeat_game():
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+
+    game.guess("w")
+    game.guess("r")
+    game.guess("o")
+    game.guess("n")
+    game.guess("g")
+
+    assert game.game_state is GameState.DEFEAT
+
+
+def test_hangman_comming_game():
+    expected_game_states = [GameState.COMING, GameState.COMING, GameState.COMING]
+
+    result_game_states = []
+    game = HangManGame(
+        word="test",
+        max_error=5,
+    )
+    result_game_states.append(game.game_state)
+    game.guess("t")
+    result_game_states.append(game.game_state)
+    game.guess("w")
+    result_game_states.append(game.game_state)
+
+    assert expected_game_states == result_game_states
