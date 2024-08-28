1dc10290e290566f2744dafbed1369bb4d92457e
Apostol Fet <maksim170901@gmail.com>
2024-08-28 22:12:48 +0300

    feat: formating message and update/delete message

--- src/hangman/presentation/bot/handlers.py
+++ src/hangman/presentation/bot/handlers.py
@@ -19,9 +19,15 @@ def start_game(
 ):
     bot.send_message(message.chat.id, localization.get_view_greateing())
     game_step = create_game_interactor(message.chat.id)
-    bot.send_message(message.chat.id, localization.get_view_game_step(game_step))
+    view_game_step = localization.get_view_game_step(game_step)
+    message_sended = bot.send_message(
+        message.chat.id,
+        f"```hangman\n{view_game_step}\n```",
+        parse_mode="Markdown",
+    )
 
     state.set(GameStates.playing)
+    state.add_data(game_step_message_id=message_sended.id)
 
 
 @inject
@@ -35,23 +41,57 @@ def guess_letter(
     user_id = message.chat.id
     letter = message.text
 
+    bot.delete_message(user_id, message.id)
+
+    with state.data() as data:
+        delete_message = data.get("delete_message")
+    if delete_message:
+        bot.delete_message(user_id, delete_message)
+        state.add_data(delete_message=None)
+
     if letter is None:
-        bot.send_message(user_id, localization.get_view_letter_error(""))
+        message_sended = bot.send_message(
+            user_id, localization.get_view_letter_error("")
+        )
+        state.add_data(delete_message=message_sended.id)
         return
 
     try:
         game_step = guess_interactor(user_id, letter)
     except LetterError as ex:
-        bot.send_message(user_id, localization.get_view_letter_error(ex.letter))
+        message_sended = bot.send_message(
+            user_id, localization.get_view_letter_error(ex.letter)
+        )
+        state.add_data(delete_message=message_sended.id)
         return
 
     view_game_step = localization.get_view_game_step(game_step)
-    bot.send_message(user_id, view_game_step)
+    question_letter = localization.get_question_letter()
+    telegram_view_game_step = f"```hangman\n{view_game_step}\n```\n{question_letter}"
+
+    with state.data() as data:
+        game_step_message_id = data.get("game_step_message_id")
+
+    if game_step_message_id:
+        bot.edit_message_text(
+            telegram_view_game_step,
+            user_id,
+            game_step_message_id,
+            parse_mode="Markdown",
+        )
+    else:
+        message_sended = bot.send_message(
+            user_id,
+            f"```hangman\n{view_game_step}\n```\n{question_letter}",
+            parse_mode="Markdown",
+        )
+        state.add_data(game_step_message_id=message_sended.id)
 
     if game_step.game_state is GameState.COMING:
         return
 
     view_end_game = localization.get_view_end_game(game_step)
+
     bot.send_message(user_id, view_end_game)
     state.delete()
 
@@ -70,14 +110,14 @@ def end_game(
 def register_handlers(bot: TeleBot):
     bot.register_message_handler(start_game, commands=["start"], pass_bot=True)
     bot.register_message_handler(
-        guess_letter,
-        func=lambda message: message.text != "",
-        pass_bot=True,
-        state=GameStates.playing,
-    )
-    bot.register_message_handler(
         end_game,
         commands=["end"],
         state=GameStates.playing,
         pass_bot=True,
+    )
+    bot.register_message_handler(
+        guess_letter,
+        func=lambda message: message.text != "",
+        pass_bot=True,
+        state=GameStates.playing,
     )
